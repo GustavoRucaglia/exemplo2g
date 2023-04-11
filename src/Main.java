b5a79578044c8778384455ff3e0d0f5a58d5b799
rm87162 <gustavorucaglia@gmail.com>
2023-04-11 08:19:01 -0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class
 Main {
     public static void main(String[] args) {
-        System.out.println("Hello worl!");
+      //laço de repetição
+        int contador = 0;
+        while (contador < 10){
+            System.out.println("contador:" + contador);
+            //incremento
+            contador++;
+        }
     }
 }
